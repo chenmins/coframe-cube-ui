22fd08b3d0ff1498f705a5db20ff3ab3913e76ba
grit <136907108@qq.com>
2021-01-14 15:11:41 +0800

    变更api代理地址

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,26 @@
+/*
+ vue.config配置 https://cli.vuejs.org/zh/config/#%E5%85%A8%E5%B1%80-cli-%E9%85%8D%E7%BD%AE
+*/
+
+
 module.exports = {
+  pages:{
+    index:{
+      entry:'src/main',
+      title:'demo'
+    }
+  },
   devServer: {
     host: "localhost",
-    port: 8081, // 端口号
+    port: 8080, // 端口号
     open: true, //配置自动启动浏览器
-
-    // 配置多个代理
     proxy: {
-      "/api": {
-        target: "http://0.0.0.0:9527/api",// 要访问的接口域名
-        ws: false,// 是否启用websockets
-        changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+      "/api": { //是否使用代理标识,/api开头的才用代理
+        target: "http://39.105.213.176/api",
+        ws: false,// 启用websockets
+        changeOrigin: true, //跨域
         pathRewrite: {
-          '^/api': ''
+          '^/api': '' // 将/api开头的请求地址的/api替换为''
         },
       }
     }
