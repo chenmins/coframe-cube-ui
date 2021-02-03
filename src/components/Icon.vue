f3a51600f0cca7be2845b1332f91d3c25482ed08
AliveCrow <136907108@qq.com>
2021-02-03 18:15:29 +0800

    修改日程协同, 根据原型设计页面，完成政务管理中心+文化建设的公告列表和企业新闻

---
+++ src/components/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg  :class="`icon ${className}`" aria-hidden="true" @click="clickIcon">
+    <use :xlink:href="'#'+svgName"/>
+  </svg>
+</template>
+
+
+<script>
+const importAll = (requireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('@/assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error,1);
+}
+
+export default {
+  name:'Icon',
+  props:[
+      'svgName',
+      'className',
+      'tagsSource'
+  ],
+  methods:{
+    clickIcon(){
+      this.$emit('iconToggle')
+    }
+  }
+
+}
+</script>
